2566066428365ae01a6c6ae23b361f95216ab28f
SinuheJardinez <sinuhe.jh@gmail.com>
2021-12-23 14:09:11 -0600

    palabras

--- src/components/home.js
+++ src/components/home.js
@@ -57,7 +57,7 @@ const home = () => {
     </div>
 
  
-    {/* <div className="relative mt-20">
+    <div className="relative mt-20">
       <div className="lg:mx-auto lg:max-w-7xl lg:px-8 lg:grid lg:grid-cols-2 lg:gap-24 lg:items-start">
         <div className="relative sm:py-16 lg:py-0">
           <div aria-hidden="true" className="hidden sm:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-screen">
@@ -74,26 +74,22 @@ const home = () => {
           <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0 lg:max-w-none lg:py-20">
         
             <div className="relative pt-64 pb-10 rounded-2xl shadow-xl overflow-hidden">
-              <img className="absolute inset-0 h-full w-full object-cover" src="https://images.unsplash.com/photo-1521510895919-46920266ddb3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&fp-x=0.5&fp-y=0.6&fp-z=3&width=1440&height=1440&sat=-100" alt=""/>
-              <div className="absolute inset-0 bg-rose-500 mix-blend-multiply"></div>
-              <div className="absolute inset-0 bg-gradient-to-t from-rose-600 via-rose-600 opacity-90"></div>
+              <img className="absolute inset-0 h-full w-full object-cover" src="https://images.squarespace-cdn.com/content/v1/6075b283247679192461db8d/1618863221429-NNRRI08NBF238J93KLNO/iStock-1136415038.jpg" alt=""/>
+              {/* <div className="absolute inset-0 bg-rose-500 mix-blend-multiply"></div>
+              <div className="absolute inset-0 bg-gradient-to-t from-rose-600 via-rose-600 opacity-90"></div> */}
               <div className="relative px-8">
-                <div>
-                  <img className="h-12" src="https://tailwindui.com/img/logos/workcation.svg?color=white" alt="Workcation"/>
-                </div>
+                
                 <blockquote className="mt-8">
-                  <div className="relative text-lg font-medium text-white md:flex-grow">
+                  {/* <div className="relative text-lg font-medium text-white md:flex-grow">
                     <svg className="absolute top-0 left-0 transform -translate-x-3 -translate-y-2 h-8 w-8 text-rose-400" fill="currentColor" viewBox="0 0 32 32" aria-hidden="true">
                       <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
                     </svg>
                     <p className="relative">
                       Tincidunt integer commodo, cursus etiam aliquam neque, et. Consectetur pretium in volutpat, diam. Montes, magna cursus nulla feugiat dignissim id lobortis amet.
                     </p>
-                  </div>
+                  </div> */}
 
-                  <footer className="mt-4">
-                    <p className="text-base font-semibold text-rose-200">Sarah Williams, CEO at Workcation</p>
-                  </footer>
+                  
                 </blockquote>
               </div>
             </div>
@@ -104,22 +100,26 @@ const home = () => {
          
           <div className="pt-12 sm:pt-16 lg:pt-20">
             <h2 className="text-3xl text-gray-900 font-extrabold tracking-tight sm:text-4xl">
-              On a mission to empower teams
+             Conoce artistas únicos y síguelos
             </h2>
             <div className="mt-6 text-gray-500 space-y-6">
               <p className="text-lg">
-                Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique pellentesque. Blandit amet, sed aenean erat arcu morbi. Cursus faucibus nunc nisl netus morbi vel porttitor vitae ut. Amet vitae fames senectus vitae.
+              Las redes sociales se han vuelto el mas grande semillero de talento creativo que ha existido en la historia de la humanidad.
               </p>
               <p className="text-base leading-7">
-                Sollicitudin tristique eros erat odio sed vitae, consequat turpis elementum. Lorem nibh vel, eget pretium arcu vitae. Eros eu viverra donec ut volutpat donec laoreet quam urna. Sollicitudin tristique eros erat odio sed vitae, consequat turpis elementum. Lorem nibh vel, eget pretium arcu vitae. Eros eu viverra donec ut volutpat donec laoreet quam urna.
+                Desde el nacimiento de influencers, bailarines de tiktok o hasta talentos artísticos como cantantes, actores o creadores multimedia, 
+                el alcance de las redes sociales esta en las posibilidades de creación multimedia.
+
               </p>
               <p className="text-base leading-7">
-                Rhoncus nisl, libero egestas diam fermentum dui. At quis tincidunt vel ultricies. Vulputate aliquet velit faucibus semper. Pellentesque in venenatis vestibulum consectetur nibh id. In id ut tempus egestas. Enim sit aliquam nec, a. Morbi enim fermentum lacus in. Viverra.
-              </p>
+               Este sitio web busca impulsar el talento independiente, el que crece en las redes sociales, el que se abre paso con sus propias manos 
+               y con su ingenio y creatividad. En esta plataforma pueden crear sus propios artículos y venderlos a sus anchas,
+               así como promover sus redes sociales e interactuar con nuevos y mas talentos artísticos!
+             </p>
             </div>
           </div>
 
-          <div className="mt-10">
+          {/* <div className="mt-10">
             <dl className="grid grid-cols-2 gap-x-4 gap-y-8">
               <div className="border-t-2 border-gray-100 pt-6">
                 <dt className="text-base font-medium text-gray-500">Founded</dt>
@@ -146,10 +146,10 @@ const home = () => {
                 Learn more about how we're changing the world
               </a>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
-    </div> */}
+    </div>
 
 
     {/* <div className="mt-32">
